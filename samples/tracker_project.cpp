03be8c7b9f624d512b61639aa7c112a1f87e3ad4
xamcaa <ss2015@ws-2k-110-n08.ccam.cmc.unn.net>
2015-08-25 12:15:56 +0300

    Code was refactoried.

--- samples/tracker_project.cpp
+++ samples/tracker_project.cpp
@@ -35,36 +35,45 @@ bool Trackerproject::track( const cv::Mat& frame, cv::Rect& new_position )
 
 	std::vector<long> possible_dy;
 	std::vector<long> possible_dx;
-	std::vector<double> ver;ver.push_back(0);ver.push_back(0);ver.push_back(0);ver.push_back(0);
+	std::vector<double> ver;
 
 	possible_dx.push_back(0);possible_dy.push_back(10);
-		possible_dx.push_back(10);possible_dy.push_back(0);
-			possible_dx.push_back(0);possible_dy.push_back(-10);
-		possible_dx.push_back(-10);possible_dy.push_back(0);
-		std::cout<<"4";
-	for(long i=0;i<possible_dx.size();i++){
-										 cv::Rect position1;cv::Mat hist1;position1=position_;
-										 position1.x+=possible_dx[i];position1.y+=possible_dy[i];
-										 cv::Mat for_hist(previous_frame,position1);
-	 int histSize[3] = {8, 8, 8};
-    float range[2] = {0, 256};
-    const float * ranges[3] = {range, range, range};
-    int channels[3] = {0, 1, 2};
-    calcHist(&for_hist, 1, channels, cv::Mat(), hist1, 3, histSize, ranges);
-	double ver1=compareHist(old_hist,hist1,CV_COMP_BHATTACHARYYA);
-	ver[i]=ver1;
-	}
+	possible_dx.push_back(10);possible_dy.push_back(0);
+	possible_dx.push_back(0);possible_dy.push_back(-10);
+	possible_dx.push_back(-10);possible_dy.push_back(0);
+	
+	for(long i=0;i<possible_dx.size();i++) ver.push_back(0);
 
-	double norm;norm=0;for(long i=0;i<ver.size();i++){norm+=ver[i];}
+	for(long i=0;i<possible_dx.size();i++){
+										 cv::Rect position1;position1=position_;
+										 cv::Mat hist1;
+										 position1.x+=possible_dx[i];
+										 position1.y+=possible_dy[i];
+										 cv::Mat for_hist(previous_frame,position1);
+										 int histSize[3] = {8, 8, 8};
+										 float range[2] = {0, 256};
+										 const float * ranges[3] = {range, range, range};
+										 int channels[3] = {0, 1, 2};
+										 calcHist(&for_hist, 1, channels, cv::Mat(), hist1, 3, histSize, ranges);
+										 double ver1=compareHist(old_hist,hist1,CV_COMP_BHATTACHARYYA);
+										 ver[i]=ver1;
+										 }
+
+	double norm;
+	norm=0;
+	for(long i=0;i<ver.size();i++){norm+=ver[i];}
 	for(long i=0;i<ver.size();i++){ver[i]/=norm;}
 
 	double dx;double dy;dx=0;dy=0;
 
 	for(long i=0;i<ver.size();i++){
-		dx+=(possible_dx[i]*ver[i]);dy+=(possible_dy[i]*ver[i]);}
+								dx+=(possible_dx[i]*ver[i]);
+								dy+=(possible_dy[i]*ver[i]);
+								}
 
 	position_.x+=dx;
 	position_.y+=dy;
+
     new_position = position_;
 	return true;
 }
