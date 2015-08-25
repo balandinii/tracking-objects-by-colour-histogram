f46f658e8da6a1180a7d5bfabc8b62a749f72e1b
xamcaa <ss2015@ws-2k-110-n08.ccam.cmc.unn.net>
2015-08-25 12:52:17 +0300

    Program works correctly now.

--- samples/tracker_project.cpp
+++ samples/tracker_project.cpp
@@ -37,25 +37,37 @@ bool Trackerproject::track( const cv::Mat& frame, cv::Rect& new_position )
 	std::vector<long> possible_dx;
 	std::vector<double> ver;
 
-	possible_dx.push_back(0);possible_dy.push_back(10);
-	possible_dx.push_back(10);possible_dy.push_back(0);
 	possible_dx.push_back(0);possible_dy.push_back(-10);
 	possible_dx.push_back(-10);possible_dy.push_back(0);
+	possible_dx.push_back(0);possible_dy.push_back(10);
+	possible_dx.push_back(10);possible_dy.push_back(0);
+
+	possible_dx.push_back(0);possible_dy.push_back(-20);
+	possible_dx.push_back(-20);possible_dy.push_back(0);
+	possible_dx.push_back(0);possible_dy.push_back(20);
+	possible_dx.push_back(20);possible_dy.push_back(0);
+
+//	std::cout<<possible_dx.size()<<"  "<<possible_dy.size()<<"  ";
 	
 	for(long i=0;i<possible_dx.size();i++) ver.push_back(0);
+
+	//std::cout<<ver.size()<<"\n";
+
+//	for(long i=0;i<possible_dx.size();i++){
+		//std::cout<<possible_dx[i]<<"  "<<possible_dy[i]<<"\n";}
 
 	for(long i=0;i<possible_dx.size();i++){
 										 cv::Rect position1;position1=position_;
 										 cv::Mat hist1;
 										 position1.x+=possible_dx[i];
 										 position1.y+=possible_dy[i];
-										 cv::Mat for_hist(previous_frame,position1);
+										 cv::Mat for_hist(frame,position1);
 										 int histSize[3] = {8, 8, 8};
 										 float range[2] = {0, 256};
 										 const float * ranges[3] = {range, range, range};
 										 int channels[3] = {0, 1, 2};
 										 calcHist(&for_hist, 1, channels, cv::Mat(), hist1, 3, histSize, ranges);
-										 double ver1=compareHist(old_hist,hist1,CV_COMP_BHATTACHARYYA);
+										 double ver1=compareHist(old_hist,hist1,CV_COMP_CORREL);
 										 ver[i]=ver1;
 										 }
 
@@ -66,15 +78,25 @@ bool Trackerproject::track( const cv::Mat& frame, cv::Rect& new_position )
 
 	double dx;double dy;dx=0;dy=0;
 
+		//for(long i=0;i<possible_dx.size();i++){
+		//std::cout<<possible_dx[i]<<"  "<<possible_dy[i]<<"\n";}
+
 	for(long i=0;i<ver.size();i++){
 								dx+=(possible_dx[i]*ver[i]);
 								dy+=(possible_dy[i]*ver[i]);
+								//std::cout<<ver[i]<<"\n";
 								}
+
+	double dl=sqrt(dx*dx+dy*dy);
+	if(dl>0.001){dx/=dl;dy/=dl;}
+	dx*=3.0;dy*=3.0;
 
 	position_.x+=dx;
 	position_.y+=dy;
+	std::cout<<dx<<"  "<<dy<<"\n";
 
     new_position = position_;
+	previous_frame=frame;
 	return true;
 }
 
